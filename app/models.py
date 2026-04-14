1445bd0ad0805523f5ecb04605fac6fde987a02a
kts2000 <57260596+kts2000@users.noreply.github.com>
2026-04-14 18:16:01 +0530

    feat(AEA_TEST_REPO-1): implement app/models.py

--- app/models.py
+++ app/models.py
@@ -1,25 +1,18 @@
 from pydantic import BaseModel
 from typing import Optional
 
-# Pydantic models
-
-class ItemBase(BaseModel):
+class Item(BaseModel):
+    id: int
     name: str
     description: Optional[str] = None
     price: float
     in_stock: bool
 
-
-class ItemCreate(ItemBase):
+class ItemCreate(Item):
     pass
 
-
-class ItemUpdate(ItemBase):
-    pass
-
-
-class ItemInDB(ItemBase):
-    id: int
-
-    class Config:
-        orm_mode = True
+class ItemUpdate(Item):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    in_stock: Optional[bool] = None
